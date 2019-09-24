ac35da2fd2500eba0e5e86fabaa3b71768476b4d
Ranika Williams <rnkprice07@gmail.com>
2019-09-24 13:08:14 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -5,12 +5,17 @@ class PetsController < ApplicationController
     erb :'/pets/index' 
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+     @pet = Pet.create(params[:pet])
+      if !params["owner"]["name"].empty?
+        @pet.owners << Owner.create(name: params["owner"]["name"])
+      end
+      @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
