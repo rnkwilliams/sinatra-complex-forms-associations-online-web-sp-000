ad8be1db9d5621d89d09b4290f162debea7984f2
Ranika Williams <rnkprice07@gmail.com>
2019-09-24 13:32:49 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,7 +25,11 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
 
     redirect to "pets/#{@pet.id}"
   end
